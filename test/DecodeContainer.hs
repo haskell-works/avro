c60aa7488e7ab56cc07e861709e096fc338278f8
Thomas M. DuBuisson <tommd@galois.com>
2016-06-22 23:37:14 -0700

    Prettier output on a test

--- test/DecodeContainer.hs
+++ test/DecodeContainer.hs
@@ -7,4 +7,9 @@ main :: IO ()
 main =
   do [file] <- getArgs
      cont <- BL.readFile file
-     print $ decodeContainer cont
+     case decodeContainer cont of
+      Left e -> print e
+      Right (s,v) ->
+        do putStrLn $ "Schema: " ++ show s
+           putStrLn "--------------------------------------------------"
+           print v
