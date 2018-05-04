ba0c860e6a2faf5f0284f9964a9246f28f6e09f3
Tikhon Jelvis <tikhon.jelvis@target.com>
2018-05-03 20:04:03 -0700

    Extended and updated test code to test the fixed way of generating default values for unions.

--- test/Avro/DefaultsSpec.hs
+++ test/Avro/DefaultsSpec.hs
@@ -10,6 +10,7 @@ import           Data.Avro.Deriving
 import           Data.Avro.Schema
 import qualified Data.Avro.Types     as Ty
 import qualified Data.HashMap.Strict as M
+import           Data.List.NonEmpty  (NonEmpty (..))
 import qualified Data.Vector         as V
 
 import           Test.Hspec
@@ -33,7 +34,7 @@ spec = describe "Avro.DefaultsSpec: Schema with named types" $ do
       msgSchema = schemaOf (undefined :: MaybeTest)
       fixedSchema = schemaOf (undefined :: FixedTag)
       defaults = fldDefault <$> fields msgSchema
-    in defaults `shouldBe` [ Just Ty.Null
+    in defaults `shouldBe` [ Just $ Ty.Union (Null :| [String]) Null Ty.Null
                            , Just $ Ty.Fixed fixedSchema "\0\42\255"
                            , Just $ Ty.Bytes "\0\37\255"
                            ]
--- test/Avro/THUnionSpec.hs
+++ test/Avro/THUnionSpec.hs
@@ -4,8 +4,12 @@
 module Avro.THUnionSpec
 where
 
+import qualified Data.List.NonEmpty as NE
+
 import           Data.Avro
 import           Data.Avro.Deriving
+import qualified Data.Avro.Schema   as Schema
+import qualified Data.Avro.Types    as Avro
 
 import           Test.Hspec
 
@@ -27,6 +31,29 @@ spec = describe "Avro.THUnionSpec: Schema with unions." $ do
                                       }
         , unionsSameFields = Right $ NotFoo { notFooStuff = "different from Foo" }
         }
+
+      field name schema def = Schema.Field name [] Nothing (Just Schema.Ascending) schema def
+      record name namespace fields =
+        Schema.Record name namespace [] Nothing (Just Schema.Ascending) fields
+      named = Schema.NamedType . Schema.TN
+
+      expectedSchema = record "Unions" (Just "haskell.avro.example")
+        [ field "scalars"    (Schema.mkUnion (NE.fromList [Schema.String, Schema.Long])) scalarsDefault
+        , field "nullable"   (Schema.mkUnion (NE.fromList [Schema.Null, Schema.Int]))    nullableDefault
+        , field "records"    (Schema.mkUnion (NE.fromList [fooSchema, barSchema]))       Nothing
+        , field "sameFields" (Schema.mkUnion (NE.fromList [named "Foo", notFooSchema]))  Nothing
+        ]
+      scalarsDefault  = Just $ Avro.Union (NE.fromList [Schema.String, Schema.Long]) Schema.String (Avro.String "foo")
+      nullableDefault = Just $ Avro.Union (NE.fromList [Schema.Null, Schema.Int])    Schema.Null   Avro.Null
+
+      fooSchema = record "Foo" Nothing [field "stuff" Schema.String Nothing]
+      barSchema = record "Bar" Nothing
+        [ field "stuff"  Schema.String Nothing
+        , field "things" (named "Foo") Nothing
+        ]
+      notFooSchema = record "NotFoo" Nothing [field "stuff" Schema.String Nothing]
+  it "produces valid schemas" $ do
+    schema'Unions `shouldBe` expectedSchema
   it "records with unions should roundtrip" $ do
     fromAvro (toAvro objA) `shouldBe` pure objA
     fromAvro (toAvro objB) `shouldBe` pure objB
