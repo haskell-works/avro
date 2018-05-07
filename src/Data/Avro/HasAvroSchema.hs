cff794ec30d8bbe86432f07f442e940069f223c5
alexeyraga <alexey.raga@gmail.com>
2018-05-07 11:13:15 +1000

    Fix encoding empty containers

--- src/Data/Avro/HasAvroSchema.hs
+++ src/Data/Avro/HasAvroSchema.hs
@@ -1,8 +1,10 @@
-{-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE ConstraintKinds #-}
-{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE ConstraintKinds      #-}
+{-# LANGUAGE FlexibleInstances    #-}
+{-# LANGUAGE ScopedTypeVariables  #-}
+{-# LANGUAGE TypeSynonymInstances #-}
 module Data.Avro.HasAvroSchema where
 
+import qualified Data.Array           as Ar
 import           Data.Avro.Schema     as S
 import           Data.Avro.Types      as T
 import qualified Data.ByteString      as B
@@ -10,22 +12,37 @@ import           Data.ByteString.Lazy (ByteString)
 import qualified Data.ByteString.Lazy as BL
 import qualified Data.HashMap.Strict  as HashMap
 import           Data.Int
+import           Data.Ix              (Ix)
+import           Data.List.NonEmpty   (NonEmpty (..))
 import qualified Data.Map             as Map
 import           Data.Monoid          ((<>))
+import           Data.Proxy
+import qualified Data.Set             as S
+import           Data.Tagged
 import           Data.Text            (Text)
 import qualified Data.Text            as Text
-import           Data.List.NonEmpty (NonEmpty(..))
 import qualified Data.Text.Lazy       as TL
-import           Data.Tagged
 import qualified Data.Vector          as V
+import qualified Data.Vector.Unboxed  as U
 import           Data.Word
-import Data.Proxy
 
 class HasAvroSchema a where
   schema :: Tagged a Type
 
 schemaOf :: (HasAvroSchema a) => a -> Type
 schemaOf = witness schema
+
+instance HasAvroSchema Word8 where
+  schema = Tagged S.Int
+
+instance HasAvroSchema Word16 where
+  schema = Tagged S.Int
+
+instance HasAvroSchema Word32 where
+  schema = Tagged S.Long
+
+instance HasAvroSchema Word64 where
+  schema = Tagged S.Long
 
 instance HasAvroSchema Bool where
   schema = Tagged S.Boolean
@@ -35,6 +52,12 @@ instance HasAvroSchema () where
 
 instance HasAvroSchema Int where
   schema = Tagged S.Long
+
+instance HasAvroSchema Int8 where
+  schema = Tagged S.Int
+
+instance HasAvroSchema Int16 where
+  schema = Tagged S.Int
 
 instance HasAvroSchema Int32 where
   schema = Tagged S.Int
@@ -85,6 +108,18 @@ instance (HasAvroSchema a) => HasAvroSchema (Maybe a) where
   schema = Tagged $ mkUnion (S.Null:| [untag (schema :: Tagged a Type)])
 
 instance (HasAvroSchema a) => HasAvroSchema [a] where
+  schema = wrapTag S.Array (schema :: Tagged a Type)
+
+instance (HasAvroSchema a, Ix i) => HasAvroSchema (Ar.Array i a) where
+  schema = wrapTag S.Array (schema :: Tagged a Type)
+
+instance HasAvroSchema a => HasAvroSchema (V.Vector a) where
+  schema = wrapTag S.Array (schema :: Tagged a Type)
+
+instance HasAvroSchema a => HasAvroSchema (U.Vector a) where
+  schema = wrapTag S.Array (schema :: Tagged a Type)
+
+instance HasAvroSchema a => HasAvroSchema (S.Set a) where
   schema = wrapTag S.Array (schema :: Tagged a Type)
 
 wrapTag :: (Type -> Type) -> Tagged a Type -> Tagged b Type
